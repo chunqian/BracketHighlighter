4aa694e7965c3abe662289f0255c3f2df3994c0d
chunqian.shen <chunqian.shen@qq.com>
2023-07-06 15:39:13 +0800

    Fix

--- bh_modules/pascalkeywords.py
+++ bh_modules/pascalkeywords.py
@@ -9,4 +9,4 @@ License: MIT
 def compare(name, first, second, bfr):
     """Differentiate 'repeat..until' from '*..end' brackets."""
     brackets = (bfr[first.begin:first.end], bfr[second.begin:second.end])
-    return (brackets[0] == "repeat") ^ (brackets[1] == "end")
+    return (brackets[0].lower() == "repeat") ^ (brackets[1].lower() == "end")
